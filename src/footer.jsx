7ab7382d945f0f82bbe4e43bfca0097c01c6c6c9
fioronifilippo <fioronifilippo@gmail.com>
2026-01-21 17:00:23 +0100

    Fix responsività mobile

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css";
 function Footer() {
   return (
     <footer id="footerContenitore">
-      <span>© Jozefina Koliqi 2025</span>
+      <span>© Jozefina Koliqi 2026</span>
       <span>Designed by me & developed by my husband Filippo Fioroni</span>
     </footer>
   );
