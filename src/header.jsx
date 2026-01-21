1da85709c438b61ba9f4a284f1fdfa6b4d52849a
fioronifilippo <fioronifilippo@gmail.com>
2026-01-21 18:07:12 +0100

    Fix responsività mobile

--- src/header.jsx
+++ src/header.jsx
@@ -1,15 +1,28 @@
-import { Link } from "react-router-dom"; // Fondamentale per non ricaricare la pagina
+import { Link, useLocation } from "react-router-dom";
 import "./header.css";
 
 function Header() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <header id="headerContenitore">
-      {/* Link alla Home */}
+      {/* Il logo riceve la classe "nascondi-logo-mobile" se NON siamo in Home */}
       <Link to="/">
-        <img src="/JozefinaKoliqi.svg" className="logo" alt="Jozefina logo" />
+        <img
+          src="/JozefinaKoliqi.svg"
+          className={`logo ${!isHome ? "nascondi-logo-mobile" : ""}`}
+          alt="Jozefina logo"
+        />
       </Link>
 
-      {/* Link alla pagina About Me */}
+      {/* Mostra la scritta HOME solo se NON siamo in Home */}
+      {!isHome && (
+        <Link to="/" className="linkHeaderHome">
+          HOME
+        </Link>
+      )}
+
       <Link to="/about-me" className="linkHeader">
         ABOUT ME
       </Link>
